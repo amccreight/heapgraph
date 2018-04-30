52d42e11a13683f0643cd2b98596b61ff4b46bfa
Andrew McCreight <continuation@gmail.com>
2018-04-30 13:14:17 -0700

    fix dom_tree by using the correct find() algorithm

--- g/dom_tree.py
+++ g/dom_tree.py
@@ -17,80 +17,148 @@ parser.add_argument('file_name',
                     help='GC graph file name')
 
 
-#######
-# union find with path compression and union by rank
+def printTree(t):
+  for x, children in t.iteritems():
+    print "{}: {}".format(x, " ".join(children))
 
-def findi (m, x):
-  if not x in m:
-    m[x] = [x, 0]
-    return m[x]
-  if m[x][0] == x:
-    return m[x]
-  z = findi (m, m[x][0])
-  m[x] = z
-  return z
+def setTree(t):
+  return frozenset([(x, frozenset(c)) for (x, c) in t.iteritems()])
 
-def find (m, x):
-  return findi(m, x)[0]
-
-def union (m, rep, x, y):
-  xp = findi (m, x)
-  yp = findi (m, y)
-  if xp == yp:
-    return
-  if xp[1] < yp[1]:
-    rep[yp[0]] = rep.get(xp[0], xp[0])
-    if xp[0] in rep:
-      del rep[xp[0]]
-    m[xp[0]][0] = yp[0]
-  elif xp[1] > yp[1]:
-    m[yp[0]][0] = xp[0]
-  else:
-    m[yp[0]][0] = xp[0]
-    m[xp[0]][1] += 1
+def checkDomTree(g, source):
+  t1 = slowDomTree(g, source)
+  t2 = domTree(g, source)
+  print "slow:", t1
+  print
+  print "fast:", t2
+  assert setTree(t1) == setTree(t2)
 
 
 ####
 
-def doStuff(g, roots):
-    tree_parent = {} # par
-    preorder_labels = ["root"] # rev
-    label_preorder = {"root": 0} # arr
-    sdom = {}
-    rev_g = {} # rg
+def computeReachable(g, source, ignore):
+  s = set()
+  def dfs(x):
+    if x == ignore:
+      return
+    s.add(x)
+    for y in g.get(x, []):
+      if not y in s:
+        dfs(y)
+  dfs(source)
+  return s
 
-    # Compute the DFS tree.
-    def dfsVisit(x_label):
-        x = len(preorder_labels)
-        preorder_labels.append(x_label)
-        label_preorder[x_label] = x
-        sdom[x] = x
-        for y_lbl in g.get(x_label, []):
-            if y_lbl == x_lbl:
-                continue
-            if not y_lbl in label_preorder:
-                dfsVisit(y_lbl)
-                y = label_preorder[y_lbl]
-                tree_parent[y] = x
-            else:
-                y = label_preorder[y_lbl]
-            rev_g.setdefault(y, []).append(x)
+# Compute the dominator tree, but using the naive algorithm.
+def slowDomTree(g, source):
+  reachable = computeReachable(g, source, None)
+  dom = {}
+  inv_dom = {}
+  # Compute the dominance relation.
+  for x in reachable:
+    for y in reachable.difference(computeReachable(g, source, x)):
+      if x != y:
+        dom.setdefault(x, []).append(y)
+        inv_dom.setdefault(y, []).append(x)
 
-    for r in roots:
-        if not r in label_preorder:
-            dfsVisit(r)
-            tree_parent[label_preorder[r]] = 0
+  # Compute the immediate dominance relation.
+  tree = {}
+  for x, dom_by in inv_dom.iteritems():
+    candidate_idom = None
+    for y in dom_by:
+      dom_y_can = candidate_idom and candidate_idom in dom.get(y)
+      dom_can_y = not candidate_idom or y in dom.get(candidate_idom, [])
+      assert dom_y_can != dom_can_y
+      if dom_y_can:
+        continue
+      candidate_idom = y
+    assert candidate_idom
+    tree.setdefault(candidate_idom, []).append(x)
 
-    # Compute the sdom and whatever the other thing is.
-    bucket = {}
-    uf_merge = {}
-    uf_rep = {}
+  return tree
 
-    for y in reversed(xrange(len(preorder_labels))):
-        for x in rev_g[y]:
-            sdom[y] = min(sdom[y], sdom[find(uf_merge, x)])
-            if y > 0:
-                bucket[sdom[y]].append(y)
+
+####
+
+def domTreeRoots(g, roots):
+  # Set up a fake node for the roots.
+  fake_root_label = "root"
+  assert not fake_root_label in g
+  g[fake_root_label] = []
+  for r in roots:
+    g[fake_root_label].append(r)
+  return domTree(g, fake_root_label)
+
+
+# Lengauer and Tarjan dominator tree algorithm.
+
+def domTree(g, source_label):
+  # Compute the DFS tree.
+  tree_parent = {} # par
+  preorder_labels = [] # rev
+  label_preorder = {} # arr
+  dom = {}
+  sdom = {}
+  rev_g = {} # rg
+
+  # Compute the DFS tree.
+  def dfsVisit(x_label):
+    x = len(preorder_labels)
+    preorder_labels.append(x_label)
+    label_preorder[x_label] = x
+    sdom[x] = x
+    dom[x] = x
+    if not x_label in g:
+      return
+    for y_label in g[x_label]:
+      if y_label == x_label:
+        continue
+      if not y_label in label_preorder:
+        dfsVisit(y_label)
+        y = label_preorder[y_label]
+        tree_parent[y] = x
+      else:
+        y = label_preorder[y_label]
+      rev_g.setdefault(y, []).append(x)
+
+  dfsVisit(source_label)
+
+  # Compute the semi-dominator and whatever the other thing is.
+  bucket = {}
+  ds_parent = {}
+  path_min = {}
+
+  def find(x):
+    if not x in ds_parent:
+      return x
+    if ds_parent[x] in ds_parent:
+      path_min[x] = min(path_min[x], find(ds_parent[x]))
+      ds_parent[x] = ds_parent[ds_parent[x]]
+    return path_min[x]
+
+  for y in reversed(xrange(1, len(preorder_labels))):
+    for x in rev_g.get(y, []):
+      sdom[y] = min(sdom[y], sdom[find(x)])
+    bucket.setdefault(sdom[y], []).append(y)
+
+    assert not y in ds_parent
+    ds_parent[y] = tree_parent[y]
+    path_min[y] = sdom[y]
+    if not tree_parent[y] in bucket:
+      continue
+    for w in bucket[tree_parent[y]]:
+      v = find(w)
+      dom[w] = v if sdom[v] < sdom[w] else tree_parent[y]
+    del bucket[tree_parent[y]]
+
+  # Do the final calculation.
+  tree = {}
+  for x in xrange(1, len(preorder_labels)):
+    if dom[x] != sdom[x]:
+      dom[x] = dom[dom[x]]
+    x_label = preorder_labels[x]
+    dom_x_label = preorder_labels[dom[x]]
+    tree.setdefault(dom_x_label, []).append(x_label)
+
+  return tree
 
 
 def loadGraph(fname):
@@ -105,11 +173,39 @@ def loadGraph(fname):
 
 
 if __name__ == "__main__":
-  #args = parser.parse_args()
-  #(g, ga) = loadGraph(args.file_name)
 
-  g = {}
-  g["a"] = ["b", "c"]
-  g["b"] = ["b1", "b2"]
+  if True:
+    args = parser.parse_args()
+    (g, ga) = loadGraph(args.file_name)
+    print domTreeRoots(g, ga.roots)
+  elif True:
+    g1 = ("c",
+          {
+            "a": ["b"],
+            "b": ["c", "d"],
+            "c": ["e"],
+            "d": ["e"],
+            "e": ["a"],
+          })
 
-  doStuff(g, ["a"])
+    # Example from https://tanujkhattar.wordpress.com/2016/01/11/dominator-tree-of-a-directed-graph/
+    g2 = ("R",
+          {
+            "R": ["C", "B", "A"],
+            "A": ["D"],
+            "B": ["E", "A", "D"],
+            "C": ["F", "G"],
+            "D": ["L"],
+            "E": ["H"],
+            "F": ["I"],
+            "G": ["I", "J"],
+            "H": ["K", "E"],
+            "I": ["K"],
+            "J": ["I"],
+            "K": ["R", "I"],
+            "L": ["H"],
+          })
+    g = g2
+    checkDomTree(g[1], g[0])
+
+
